db7b93fc97d60d2a9f97fccae57fe4c1c8e18725
zamharrad <mhmdzamhar@gamil.com>
2020-08-13 13:00:09 +0530

    resum page

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
 
 class Resume extends Component {
   render() {
     return (
       <div>
-        <h1>Resume Page</h1>
+        <Grid>
+          <Cell col={4}>
+            <div style={{ textAlign: "center" }} className="">
+              <img
+                src="https://www.shareicon.net/data/128x128/2017/07/13/888372_man_512x512.png"
+                alt="avatar" style={{height:'200px'}}
+              />
+            </div>
+          </Cell>
+          <Cell className="resume-right-col" col={8}>
+            Right side
+          </Cell>
+        </Grid>
       </div>
     );
   }
